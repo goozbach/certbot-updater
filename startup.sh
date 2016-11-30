7b2bd4dbb0314531575bd9e967f7211549e2b596
Derek Carter <derek@goozbach.com>
2016-11-30 18:35:29 +0000

    changing to webroot for linked volume

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ mkdir -p /etc/letsencrypt
 
 while true
 do
-  certbot certonly --standalone -n --domains rishi.goozbach.net -m derek@goozbach.com --agree-tos
+  certbot certonly --webroot -n --domains rishi.goozbach.net,blog.goozbach.net,gitlab.goozbach.net -m derek@goozbach.com --agree-tos --expand --webroot-path /var/www/html
   sleep 30d
 done
