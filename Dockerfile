36e175217d297e9f0db45037d2286e464f583776
Derek Carter <derek.carter@endurance.com>
2017-01-16 16:52:24 -0700

    changing to standalone method

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,11 @@ apt-get install -y certbot -t jessie-backports && \
 apt-get clean && \
 find /var/lib/apt/lists /tmp /var/tmp /var/log -type f -delete
 
+ENV CERTBOT_DOMAINS rishi.goozbach.net,blog.goozbach.net,gitlab.goozbach.net
+ENV ACCOUNT_EMAIL derek@goozbach.com
+ENV CERTBOT_METHOD standalone
+ENV CERTBOT_WEBROOT /var/www/html
+
 EXPOSE 80 443
 
 ADD startup.sh /startup.sh
--- startup.sh
+++ startup.sh
@@ -4,8 +4,29 @@ echo "this is startup"
 
 mkdir -p /etc/letsencrypt
 
+CERTBOT_DOMAINS=${MYDOMAINS:-rishi.goozbach.net,blog.goozbach.net,gitlab.goozbach.net}
+ACCOUNT_EMAIL=${ACCOUNT_EMAIL:-derek@goozbach.com}
+
+CERTBOT_METHOD=${CERTBOT_METHOD:-webroot}
+CERTBOT_WEBROOT=${CERTBOT_WEBROOT:-/var/www/html}
+
+case ${CERTBOT_METHOD} in
+    webroot)
+        CMD_METHOD='--webroot'
+        CMD_ARGS="--webroot-path ${CERTBOT_WEBROOT}"
+    ;;
+    standalone)
+        CMD_METHOD='--standalone'
+        CMD_ARGS=''
+    ;;
+    *)
+        echo "Unsupported method in CERTBOT_METHOD: ${CERTBOT_METHOD}"
+        exit 255
+    ;;
+esac
+
 while true
 do
-  certbot certonly --webroot -n --domains rishi.goozbach.net,blog.goozbach.net,gitlab.goozbach.net -m derek@goozbach.com --agree-tos --expand --webroot-path /var/www/html
+  certbot certonly ${CMD_METHOD} -n --domains ${CERTBOT_DOMAINS} -m ${ACCOUNT_EMAIL} --agree-tos --expand ${CMD_ARGS}
   sleep 30d
 done
